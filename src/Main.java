bea17db624a9775c3ead4b5ee7e9fcd72638726c
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2024-10-31 19:49:16 -0300

    Delete Main file - Deleted for the code migration to the folder of linkedlists

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import LinkedLists.SinglyLinkedList;
-
-public class Main {
-    public static void main(String[] args) {
-        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
-        list.insertStart(1);
-        list.print();
-    }
-}

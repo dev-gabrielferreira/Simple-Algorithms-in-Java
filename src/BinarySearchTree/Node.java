8e1e9d0497cfcbfd1a4a2fd7d06c174cf2a5b8be
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2024-11-20 12:18:13 -0300

    Update Node class -Updated Node class adding constructors to it. It has been added an empty constructor and a constructor with "value" parameter

--- src/BinarySearchTree/Node.java
+++ src/BinarySearchTree/Node.java
@@ -4,4 +4,12 @@ public class Node {
     public int value;
     public Node left;
     public Node right;
+
+    public Node(){
+        super();
+    }
+
+    public Node(int value) {
+        this.value = value;
+    }
 }
